1a91cefe67dd64e29b8146fc6b7d40510fe621cd
DarkXavy <darkjav19@gmail.com>
2025-01-08 17:21:25 -0500

    More user data was entered

--- input_print.py
+++ input_print.py
@@ -1,6 +1,10 @@
 nombre = input("Ingrese su nombre:")
+segundo_nombre = input("Ingrese su segundo nombre:")
+primer_apellido = input("Ingrese su primer apellido:")
+segundo_apellido = input("Ingrese su segundo apellido:")
 
 year = int(input("Ingrese su año de nacimiento:"))
 
 print("Calculo tu edad")
-print(f"Nombre: {nombre} Edad: {2024-year}")
+print(f"Nombre Completo: {nombre + " " + segundo_nombre + " " + primer_apellido + " " + segundo_apellido},  
+      Edad: {2024-year}")
